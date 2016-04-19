4a76b744b87bb30fd6c067b1a5c3c10fb4dec727
Zachary Friedlander <zachary.d.friedlander@gmail.com>
2016-04-19 11:45:53 +0200

    Added picklejar option to attack_comparison, and changed Notebook to use YAML file

--- attack_comparison.py
+++ attack_comparison.py
@@ -9,6 +9,7 @@ def compare_to_random_networks(network, fractions, pfile):
     the network, as well as its Erdos-Renyi and Configuration model
     counterparts. Pickles all data into the given pickle filename
     """
+    print "Attacking network..."
     rnd, tgt = attack_comparison_async(network, fractions)
 
     # Create an Erdos-Renyi graph with the same number of nodes and edges
@@ -16,11 +17,13 @@ def compare_to_random_networks(network, fractions, pfile):
     M = network.number_of_edges()
     er = networkx.generators.gnm_random_graph(N, M)
 
+    print "Attacking ER network..."
     er_rnd, er_tgt = attack_comparison_async(er,fractions)
 
     # Create a configuration-model graph with the same degree sequence
     config = networkx.generators.configuration_model(network.degree().values(),
                                                   create_using=networkx.Graph())
+    print "Attacking Config network..."
     cfg_rnd, cfg_tgt = attack_comparison_async(config, fractions)
 
     # Pickle all information
